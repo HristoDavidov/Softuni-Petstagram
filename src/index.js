bb301a282edcfad5b68ed8fdfab53f6cab6dd067
HristoDavidov <hristo_davidov@mail.bg>
2024-03-03 19:01:26 +0100

    Install and setup express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Server start');
+const express = require('express');
+
+const routes = require('./routes');
+
+const PORT = 5000;
+
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
+
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT}...`));
