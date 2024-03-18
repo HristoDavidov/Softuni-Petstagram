0ab66bcd9235dd62450f1b1fc5fbcc3ab826f72b
HristoDavidov <hristo_davidov@mail.bg>
2024-03-18 12:58:08 +0100

    validate repeat password and  if user already exists

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,7 +1,15 @@
 const User = require('../models/User');
 
-exports.login = ( username, password ) => {
+exports.login = (username, password) => {
 
 };
 
-exports.register = ( userData) => User.create(userData);
+exports.register = (userData) => {
+    const user = User.findOne({ username: userData.username });
+
+    if (user) {
+        throw new Error('Username already exists!');
+    };
+
+    return User.create(userData);
+};
