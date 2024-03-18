49d5111574d1582279585a937d0207048cac3d70
HristoDavidov <hristo_davidov@mail.bg>
2024-03-18 11:06:01 +0100

    Authentication

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 //TODO: add controller routes
 const homeController = require('./controllers/homeController');
+const userController = require('./controllers/userController');
 
 
 router.use(homeController);
+router.use('/users', userController);
 
 module.exports = router;
