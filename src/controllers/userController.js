bc81874e76dca1d53b8f1edd5bf53b41c32cd03b
HristoDavidov <hristo_davidov@mail.bg>
2024-03-18 17:47:44 +0100

    Return token in cookie

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,8 +10,9 @@ router.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     const token = await userManager.login(username, password);
+    res.cookie('token', token);
 
-    res.send('Logged in');
+    res.redirect('/');
 });
 
 router.get('/register', (req, res) => {
@@ -23,7 +24,7 @@ router.post('/register', async (req, res) => {
 
     await userManager.register({ username, email, password, repeatPassword });
 
-    res.send('registered');
+    res.redirect('/users/login');
 
 });
 
