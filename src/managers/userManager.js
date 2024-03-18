b68a69ef4c7c4ed1b5455148592d1db786d95774
HristoDavidov <hristo_davidov@mail.bg>
2024-03-18 14:04:45 +0100

    Generate jwt token

--- src/managers/userManager.js
+++ src/managers/userManager.js
@@ -1,22 +1,34 @@
 const User = require('../models/User');
-
+const jwt = require('../lib/jwt');
 const bcrypt = require('bcrypt');
+
+const SECRET = '0ada2b82-ca46-4d69-88ae-1c936c27635a';
 
 exports.login = async (username, password) => {
 
     //find user by username
     const user = await User.findOne({ username });
 
-    if(!user){
+    if (!user) {
         throw new Error('Invalid user or password!');
     };
 
     //check password
     const isValid = await bcrypt.compare(password, user.password);
 
-    if(!isValid){
-        throw new Error ('Invalid user or password!');
+    if (!isValid) {
+        throw new Error('Invalid user or password!');
     };
+
+    const payload = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+    };
+
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '2d' });
+
+    return token;
 
 };
 
