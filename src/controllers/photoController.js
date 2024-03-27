7f5cfc14bb44c52ca00cb86112037deb8a64e6e8
HristoDavidov <hristo_davidov@mail.bg>
2024-03-27 10:29:26 +0100

    change PORT, add Photo model and create page

---
+++ src/controllers/photoController.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+router.get('/create', (req,res) =>{
+    res.render('photos/create');
+});
+
+
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const { errorHandler } = require('./middlewares/errorHandlerMiddleware');
 const routes = require('./routes');
 
 
-const PORT = 5000;
+const PORT = 3000;
 
 const app = express();
 
