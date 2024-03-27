820a3bcd82449e557b1ef6516fcd75756aed9804
HristoDavidov <hristo_davidov@mail.bg>
2024-03-27 11:23:51 +0100

    Add photoController and Manager

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -1,7 +1,25 @@
 const router = require('express').Router();
 
-router.get('/create', (req,res) =>{
+const photoManager = require('../managers/photoManager');
+const { getErrorMessage } = require('../utils/errorHelpers');
+
+router.get('/create', (req, res) => {
     res.render('photos/create');
+});
+
+router.post('/create', async (req, res) => {
+    const photoData = {
+        ...req.body,
+        owner: req.user._id,
+    };
+
+    try {
+        await photoManager.create(photoData);
+
+        res.redirect('/photos');
+    } catch (err) {
+        res.render('photos/create', { error: getErrorMessage(err)});
+    }
 });
 
 
