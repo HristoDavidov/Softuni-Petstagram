95b91061bd0b4caa3980d0dbe2e0f1249c9fda36
HristoDavidov <hristo_davidov@mail.bg>
2024-03-27 15:21:24 +0100

    Add details and  edit page

--- src/controllers/photoController.js
+++ src/controllers/photoController.js
@@ -28,6 +28,48 @@ router.post('/create', async (req, res) => {
     }
 });
 
+router.get('/:photoId/details', async (req, res) => {
+    const photoId = req.params.photoId;
+    const photo = await photoManager.getOne(photoId).lean();
+    const isOwner = req.user?._id == photo.owner._id;
+
+    res.render('photos/details', { photo, isOwner });
+});
+
+router.get('/:photoId/delete', async (req, res) => {
+    const photoId = req.params.photoId;
+
+    try {
+        await photoManager.delete(photoId);
+        res.redirect('/photos');
+    } catch (err) {
+        res.render('photos/details', { error: 'Unsuccessful photo deletion !' })
+    }
+
+});
+
+router.get('/:photoId/edit', async (req, res) => {
+
+    const photoId = req.params.photoId;
+    const photo = await photoManager.getOne(photoId).lean();
+    res.render('photos/edit', { photo });
+});
+
+router.post('/:photoId/edit', async (req, res) => {
+    const photoId = req.params.photoId;
+    const photoData = req.body;
+    try {
+
+        await photoManager.edit(photoId, photoData);
+
+        res.redirect(`/photos/${photoId}/details`);
+
+    } catch (err) {
+        res.render('photos/edit', { error: 'Unable to update photo!', ...photoData })
+    }
+
+});
+
 
 
 module.exports = router;
--- src/managers/photoManager.js
+++ src/managers/photoManager.js
@@ -3,3 +3,9 @@ const Photo = require('../models/Photo');
 exports.create = (photoData) => Photo.create(photoData);
 
 exports.getAll = () => Photo.find().populate('owner');
+
+exports.getOne = (photoId) => Photo.findById(photoId).populate('owner');
+
+exports.delete = (photoId) => Photo.findByIdAndDelete(photoId);
+
+exports.edit = (photoId, photoData) => Photo.findByIdAndUpdate(photoId, photoData);
