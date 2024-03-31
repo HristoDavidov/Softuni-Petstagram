8dfdaca3b2609ffab01fd8f5aecff3cbf647fedf
HristoDavidov <hristo_davidov@mail.bg>
2024-03-31 12:54:33 +0200

    Add profile page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,22 @@
 const router = require('express').Router();
 
+const photoManager = require('../managers/photoManager');
+
 router.get('/', (req, res) => {
 
     res.render('home');
 });
 
 router.get('/404', (req, res) => {
-    
+
     res.render('404');
+});
+
+router.get('/profile', async (req, res) => {
+
+    const photos = await photoManager.getByOwner(req.user._id).lean();
+
+    res.render('profile', { photos, photoCount : photos.length });
 });
 
 module.exports = router;
